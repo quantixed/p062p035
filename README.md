383caec0a8e3e599ed1ca67d3ea0b1afb4f2dd5b
Stephen Royle <s.j.royle@warwick.ac.uk>
2025-02-05 15:23:56 +0000

    Modified Fig S5

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ Data and code for manuscript `p062p035`
 
 James Shelford\*, Selena G. Burgess\*, Elena Rostkova, Mark W. Richards, Gabrielle Larocque, Josephina Sampson, Christian Tiede, Alistair J. Fielding, Tina Daviter, Darren C. Tomlinson, Antonio N. Calabrese, Mark Pfuhl\*\*, Richard Bayliss\*\* and Stephen J. Royle\*\*
 
-*bioRxiv* 2024. DOI: [https://doi.org/10.1101/2024.05.31.596836](https://doi.org/10.1101/2024.05.31.596836)
+*J Cell Biol* 2025. in press.
+
+Preprint DOI: [https://doi.org/10.1101/2024.05.31.596836](https://doi.org/10.1101/2024.05.31.596836)
 
 ## Code and data for recreation of plots
 
--- plots/gl_noc/Script/script.R
+++ plots/gl_noc/Script/script.R
@@ -1,5 +1,33 @@
 library(ggplot2)
 library(cowplot)
+library(patchwork)
+
+## Functions ----
+
+pie_maker <- function(x1, cols) {
+  df <- data.frame(
+    pvalues = x1,
+    pcolours = as.factor(seq_along(cols))
+  )
+  cs <- cumsum(x1)
+  df$ymin <- c(0, cs[seq_along(cs) - 1])
+  df$ymax <- cs
+  
+  p <- ggplot() +
+    geom_rect(data = df, aes(fill = pcolours,
+                             ymin = ymin, ymax = ymax, xmin = 0, xmax = 3)) +
+    scale_fill_manual(values = cols) +
+    xlim(c(0, 3)) +
+    theme_void() + # switch to minimal theme if you need to check values
+    theme(aspect.ratio = 1) +
+    coord_polar(theta = "y") +
+    theme(legend.position = "none")
+  
+  return(p)
+}
+
+## Script ----
+
 df <- read.csv("Data/quantifall.csv")
 # convert to long format, foci is column headers become variable names
 df_long <- reshape2::melt(df, id.vars = c("foci","class"))
@@ -29,4 +57,25 @@ ggplot(df_long, aes(x = foci, y = value, fill = class)) +
   theme(legend.position = "none")
 ggsave("Output/Plots/noc.pdf", width = 80, height = 60, units = "mm")
 
+ggplot(df_long, aes(x = foci, y = value, fill = class)) +
+  geom_bar(stat = "identity", position = "stack") +
+  scale_fill_manual(values = c("#00BFC4", "#F8766D", "#F8766d")) +
+  lims(y = c(0,100)) +
+  labs(x = "PCNT foci", y = "Percentage of cells") +
+  facet_grid(treat~prot) +
+  theme_cowplot(9) +
+  theme(legend.position = "none")
+ggsave("Output/Plots/noc_rev.pdf", width = 80, height = 60, units = "mm")
 
+# make pie charts
+df_pie <- df_long[df_long$foci == 2,]
+my_cols <- c("#F8766D", "#F8766d7f")
+# use lapply to make pie charts for all the other treatments
+pie_list <- lapply(unique(df_pie$variable), function(x) {
+  pie_maker(df_pie$value[df_pie$variable == x], my_cols)
+})
+# use patchwork to make a single plot of all pies
+design <- "ACE
+BDF"
+q <- wrap_plots(pie_list, design = design)
+ggsave("Output/Plots/pie.pdf", q, width = 40, height = 30, units = "mm")
